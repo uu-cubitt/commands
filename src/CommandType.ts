e5adf7be78ada190c40f2bbf025e0d318e7aa74b
Thomas Ipskamp <thomas.ipskamp@gmail.com>
2016-04-22 15:20:49 +0200

    implemented a string based CommandType that behaves as would be expected of an enum

--- src/CommandType.ts
+++ src/CommandType.ts
@@ -1,21 +1,43 @@
 /**
  * An enum of all types of commands.
  */
-export enum CommandType {
-	AddConnector,
-	AddEdge,
-	AddModel,
-	AddNode,
-	DeleteConnector,
-	DeleteEdge,
-	DeleteModel,
-	DeleteNode,
-	SetConnectorProperty,
-	SetEdgeProperty,
-	SetModelProperty,
-	SetNodeProperty,
-	DeleteConnectorProperty,
-	DeleteEdgeProperty,
-	DeleteModelProperty,
-	DeleteNodeProperty
-}
+export type CommandType = "AddConnectorCommand" | "AddEdgeCommand" | "AddModelCommand" | "AddNodeCommand" | "DeleteConnectorCommand" | "DeleteEdgeCommand" | "DeleteModelCommand" | "DeleteNodeCommand" | "SetConnectorPropertyCommand" | "SetEdgePropertyCommand" | "SetModelPropertyCommand" | "SetNodePropertyCommand" | "DeleteConnectorPropertyCommand" | "DeleteEdgePropertyCommand" | "DeleteModelPropertyCommand" | "DeleteNodePropertyCommand";
+
+/**
+ * An enum of all types of commands.
+ */
+export const CommandType: {
+	AddConnector: CommandType,
+	AddEdge: CommandType,
+	AddModel: CommandType,
+	AddNode: CommandType,
+	DeleteConnector: CommandType,
+	DeleteEdge: CommandType,
+	DeleteModel: CommandType,
+	DeleteNode: CommandType,
+	SetConnectorProperty: CommandType,
+	SetEdgeProperty: CommandType,
+	SetModelProperty: CommandType,
+	SetNodeProperty: CommandType,
+	DeleteConnectorProperty: CommandType,
+	DeleteEdgeProperty: CommandType,
+	DeleteModelProperty: CommandType,
+	DeleteNodeProperty: CommandType
+} = {
+	AddConnector: "AddConnectorCommand" as CommandType,
+	AddEdge: "AddEdgeCommandCommand" as CommandType,
+	AddModel: "AddModelCommand" as CommandType,
+	AddNode: "AddNodeCommand" as CommandType,
+	DeleteConnector: "DeleteConnectorCommand" as CommandType,
+	DeleteEdge: "DeleteEdgeCommand" as CommandType,
+	DeleteModel: "DeleteModelCommand" as CommandType,
+	DeleteNode: "DeleteNodeCommand" as CommandType,
+	SetConnectorProperty: "SetConnectorPropertyCommand" as CommandType,
+	SetEdgeProperty: "SetEdgePropertyCommand" as CommandType,
+	SetModelProperty: "SetModelPropertyCommand" as CommandType,
+	SetNodeProperty: "SetNodePropertyCommand" as CommandType,
+	DeleteConnectorProperty: "DeleteConnectorPropertyCommand" as CommandType,
+	DeleteEdgeProperty: "DeleteEdgePropertyCommand" as CommandType,
+	DeleteModelProperty: "DeleteModelPropertyCommand" as CommandType,
+	DeleteNodeProperty: "DeleteNodePropertyCommand" as CommandType
+};
